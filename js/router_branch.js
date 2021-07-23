ab45674ae03c12b4eb31723387f58384d44a3b96
nhquang <quangpersie1@gmail.com>
2021-07-23 00:35:30 -0400

    more routes for branch

--- js/router_branch.js
+++ js/router_branch.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const Department = require('../models/BranchModel');
+const Branch = require('../models/BranchModel');
 
 router.get("/", (req,res) => {
-    Department
+    Branch
     .find()
     .exec()
     .then((data) => {
@@ -14,8 +14,28 @@ router.get("/", (req,res) => {
     });
 });
 
+router.get('/active_branches', (req, res) => {
+    Branch
+    .find({active: true})
+    .exec()
+    .then((list)=>{
+        res.json(list);
+    })
+    .catch((err) => { console.log("An error occurred: ${err}" + err) });
+});
+
+router.get('/inactive_branches', (req, res) => {
+    Branch
+    .find({active: false})
+    .exec()
+    .then((list)=>{
+        res.json(list);
+    })
+    .catch((err) => { console.log("An error occurred: ${err}" + err) });
+});
+
 router.get("/:id", (req,res) =>{
-    Department
+    Branch
     .findOne({_id: req.params.id})
     .exec()
     .then((data) => {
@@ -23,6 +43,30 @@ router.get("/:id", (req,res) =>{
     })
     .catch(() => {
         console.log(`An error occurred: ${err}`);
+    });
+});
+
+router.put('/update_branch/:id', (req, res) => {
+    Branch.findOneAndUpdate({_id: req.params.id},
+        req.body, {new: true},(err, data)=>{
+            if (err) {
+                res.status(400).json({ message: err });
+            }
+            else {
+                res.status(200).json(data);
+            }
+        }
+    );
+});
+
+router.delete('/delete_branch/:id', (req, res) => {
+    Branch.deleteOne({ _id: req.params.id },(err, member)=>{
+        if (err) {
+            res.status(400).json({ message: err });
+        }
+        else {
+            res.json(req.params.id + " deleted!!!");
+        }
     });
 });
 
