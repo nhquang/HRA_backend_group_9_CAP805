32ac65de16336e24e0551aa33d710bdf4684a13e
nhquang <quangpersie1@gmail.com>
2021-07-25 13:57:09 -0400

    user_authentication, authorization from Khoa and a major fix from me

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require('body-parser');
 const dotenv = require("dotenv");
+const EmployeeModel = require('./models/EmployeeModel');
+const jwt = require('jsonwebtoken');
 const HTTP_PORT = process.env.PORT || 8080;
 
 dotenv.config();
@@ -33,9 +35,10 @@ mongoose.connect(
     });
 
     app.use(bodyParser.json());
+    app.use('/authenticate', require('./js/router_authentication'));
     app.use('/departments', require('./js/router_department'));
     app.use('/branches', require('./js/router_branch'));
-    app.use('/employees', require('./js/router_employee'));
+    app.use('/employees', verify, require('./js/router_employee'));
 
     app.use((req, res, next) => {
         res.status(404).send("Not Found");
@@ -47,3 +50,35 @@ mongoose.connect(
 }).catch((err) => {
     console.log(err.message);
 });
+
+function verify(req,res,next){
+    const token = req.headers['authorization'].substring(7);
+    console.log(token);
+    if (token) {
+        jwt.verify(token, process.env.jwt_secret, function(err, decoded) {
+            if (err) res.status(401).json({ message: "Invalid token!"});
+            else{
+                EmployeeModel
+                .findOne({username: decoded.username})
+                .exec()
+                .then((data)=>{
+                    if(data){
+                        req.decoded = decoded;
+                        next();
+                    }
+                    else res.status(401).send({ message: "Invalid token!"});
+                })
+                .catch((err) => {
+                    console.log("An error occurred: ${err}" + err);
+                    res.status(500).json({
+                        message: "Internal Server Error!"
+                    });
+                });
+            }
+        });
+    } else {
+        res.status(401).json({
+            message:"Unauthorized access!"
+        });
+    }
+}
