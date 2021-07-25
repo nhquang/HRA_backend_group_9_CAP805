fe35dd25f7f878705aeecd44f759c3234dbc003c
zfkhrl@gmail.com <zfkhrl@gmail.com>
2021-07-25 18:36:01 -0400

    add update_account

--- js/router_account.js
+++ js/router_account.js
@@ -1,7 +1,9 @@
-const User = require('../models/EmployeeModel');
+const EmployeeModel = require('../models/EmployeeModel');
 const express = require('express');
 const router = express.Router();
 const dotenv = require("dotenv");
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 
 dotenv.config();
 
@@ -9,10 +11,47 @@ router.get('/',(req,res) =>{
     res.json({username: req.decoded.username});
 });
 
-router.put('/update_account', (req,res)=>{
-    //TODO
-    //
-    //
+router.post('/update_account'
+    , body('username').notEmpty().bail().custom((value, { req }) => {
+        if(value!=req.decoded.username){
+            return EmployeeModel.findOne({username: value}).then(user => {
+                if(user){
+                    return Promise.reject('username exists, please choose another username');
+                }
+            });
+        }
+        return true;
+    })
+    , body('password', ).isLength({ min: 6 })
+    , body('confirmPassword', ).custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Password confirmation does not match password');
+        }
+        // Indicates the success of this synchronous custom validator
+        return true;
+    })
+    , (req,res)=>{
+
+    const oldUserName = req.decoded.username;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    bcrypt.hash(password, 10, function(err, hash) {
+        EmployeeModel.findOneAndUpdate({username: oldUserName},
+            {username: username, password: hash}, null,(err, data)=>{
+                if (err) {
+                    res.status(400).json({ message: err });
+                }else {
+                    res.status(200).json({ message: "update account success" });
+                }
+            }
+        );
+    });
 });
 
 module.exports = router;
