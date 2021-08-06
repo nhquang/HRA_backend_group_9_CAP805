e875fc7d0d53625f275eb4aa0f336c7e81ae8575
nhquang <quangpersie1@gmail.com>
2021-08-06 13:27:15 -0400

    more tests

--- test/router_authentication.test.js
+++ test/router_authentication.test.js
@@ -6,9 +6,11 @@ const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 dotenv.config();
 
-var testCredentials = null;
+var testCredentialsPass = null;
+var testCredentialsFail = null;
 beforeAll(async () =>{
-    testCredentials = {username: "nhquang", password: "testing123"};
+    testCredentialsPass = {username: "nhquang", password: "testing123"};
+    testCredentialsFail = {username: "random", password:"testing123"};
     await mongoose.connect(
         process.env.DB_CONNECTION_STRING,
         {useNewUrlParser: true, useUnifiedTopology: true}
@@ -24,11 +26,24 @@ describe("Test login", () =>{
     test("This test should successfully authenticate the user and receives a response with a token", done =>{
         request(app)
         .post("/authenticate/login")
-        .send(testCredentials)
+        .send(testCredentialsPass)
         .then(response => {
             //console.log(response);
             expect(response.statusCode).toBe(200);
             expect(response.body.message).toEqual("Succeeded!");
+            done();
+        });
+    });
+});
+describe("Test login", () =>{
+    test("This test should fail to authenticate the user and receives a response with error", done =>{
+        request(app)
+        .post("/authenticate/login")
+        .send(testCredentialsFail)
+        .then(response => {
+            //console.log(response);
+            expect(response.statusCode).toBe(200);
+            expect(response.body.message).toEqual("Failed!");
             done();
         });
     });
