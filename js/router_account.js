7d3420cd55e0568bc1e9a6105be8095bce614d34
zfkhrl@gmail.com <zfkhrl@gmail.com>
2021-08-12 23:55:33 -0400

    update error message

--- js/router_account.js
+++ js/router_account.js
@@ -38,7 +38,7 @@ router.post('/update_account'
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ message: "invalid parameters", errors: errors.array() });
     }
 
     bcrypt.hash(password, 10, function(err, hash) {
